c4e47af67a1f28de4bc1c2fdccad1bfdeebaddc6
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-06 14:52:21 +0900

    deposit()/withdraw() call

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -5,13 +5,30 @@ public class Account {
 	private String acctId;
 	private int balance;
 	
-	public void setBalance(int balance) {
+	//default constructor 선언
+	//개발자가 명시적으로 생성자를 선언해주지 않았을 때 기본 생성자를 생성해
+	public Account() {
+		System.out.println("기본생성자 호출됨");
+	}
+	
+	//멤버변수 초기화 시키는 생성자
+	//Constructor Overloading (생성자 중복 정의)
+	public Account(String custId, String acctId, int balance) {
+//		this.custId = custId;
+//		this.acctId = acctId;
+		setCustId(custId);
+		setAcctId(acctId);
 		this.balance = balance;
 	}
+	
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
 	
 	public int getBalance() {
 		return balance;
 	}
+	
 
 	public String getCustId() {
 		return custId;
